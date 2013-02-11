471640b00ec18244d420f908bfe21d7cb5aa0374
Nana Kwame <nanakwame123@gmail.com>
2013-02-11 16:46:15 +0000

    twitter integration underway

--- social/models.py
+++ social/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+from django.contrib.sites.models import Site 
+
+class TwitterProfile(models.Model):
+    user = models.ForeignKey(User)
+    site = models.ForeignKey(Site, default=Site.objects.get_current)
+    twitter_id = models.PositiveIntegerField()
+    
+    def __unicode__(self):
+        return u'%s: %s' % (self.user, self.twitter_id)
+    
+    def authenticate(self):
+        return authenticate(twitter_id=self.twitter_id)
+
