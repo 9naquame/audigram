37fbd41152f142f8b4289a671ea5b75881756811
Nana Kwame <nanakwame123@gmail.com>
2013-02-14 16:57:14 +0000

    model for openID added

--- social/models.py
+++ social/models.py
@@ -15,3 +15,34 @@ class TwitterProfile(models.Model):
     def authenticate(self):
         return authenticate(twitter_id=self.twitter_id)
 
+class OpenIDProfile(models.Model):
+    user = models.ForeignKey(User)
+    site = models.ForeignKey(Site, default=Site.objects.get_current)
+    identity = models.TextField()
+    
+    def __unicode__(self):
+        return u'OpenID Profile for %s, via provider %s' % (self.user, self.identity)
+
+    def authenticate(self):
+        return authenticate(identity=self.identity)
+
+class OpenIDStore(models.Model):
+    site = models.ForeignKey(Site, default=Site.objects.get_current)
+    server_url = models.CharField(max_length=255)
+    handle = models.CharField(max_length=255)
+    secret = models.TextField()
+    issued = models.IntegerField()
+    lifetime = models.IntegerField()
+    assoc_type = models.TextField()
+
+    def __unicode__(self):
+        return u'OpenID Store %s for %s' % (self.server_url, self.site)
+
+class OpenIDNonce(models.Model):
+    server_url = models.CharField(max_length=255)
+    timestamp = models.IntegerField()
+    salt = models.CharField(max_length=255)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return u'OpenID Nonce for %s' % self.server_url
