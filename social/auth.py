a9a0020ce6f04cb2a8752b7b035cbcc74b78fde7
Nana Kwame <nanakwame123@gmail.com>
2013-02-15 16:43:14 +0000

    authentification file added

---
+++ social/auth.py
@@ -0,0 +1,31 @@
+from django.contrib.auth.models import User
+from django.contrib.sites.models import Site
+
+from social.models import (TwitterProfile, OpenIDProfile)
+
+class Auth(object):
+    def get_user(self, user_id):
+        try:
+            return User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            return None
+
+class TwitterAuth(Auth):
+    def authenticate(self, twitter_id=None):
+        try:
+            return TwitterProfile.objects.get(
+                twitter_id=twitter_id,
+                site=Site.objects.get_current()
+            ).user
+        except TwitterProfile.DoesNotExist:
+            return None
+
+class OpenIDAuth(Auth):
+    def authenticate(self, identity=None):
+        try:
+            return OpenIDProfile.objects.get(
+                identity=identity,
+                site=Site.objects.get_current()
+            ).user
+        except OpenIDProfile.DoesNotExist:
+            return None
